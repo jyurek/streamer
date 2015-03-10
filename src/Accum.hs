790cbf6ac63f20caeefc1ed44ed79f5d09699ebe
Jon Yurek <jyurek@thoughtbot.com>
2015-03-10 10:28:01 -0400

    it compiles!

--- src/Accum.hs
+++ src/Accum.hs
@@ -1,45 +1,73 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Accum where
 
+import Control.Applicative
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Resource (runResourceT)
 import Data.ByteString (ByteString)
 import Data.Conduit
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Conduit.Binary as CB
-import qualified Data.Conduit.Combinators as CC
+import qualified Data.Conduit.List as CL
+
+data Progress = Continue | EndSection | EndContent
 
 takeUntil :: MonadIO m
           => ByteString
-          -> Conduit ByteString m ByteString
+          -> Conduit ByteString m (Progress, ByteString)
 takeUntil boundary = do
-    bs <- CB.take $ B.length boundary
+    let bl = B.length boundary
+    lbs <- CB.take $ bl * 3
+    let bs = toStrict lbs
 
-    liftIO $ do
-        putStr "takeUntil: "
-        BL.putStrLn bs
-
-    case () of
-        _ | BL.null bs -> return ()
-          | toStrict bs == boundary -> return ()
-          | otherwise -> do
-              yield $ toStrict $ BL.singleton $ BL.head bs
-              leftover $ toStrict $ BL.tail bs
-
-              takeUntil boundary
-
-sendEach :: MonadIO m
-         => ByteString
-         -> ResumableSource m ByteString
-         -> Sink ByteString m ()
-         -> m ()
-sendEach boundary source sink =  do
-    (s, b) <- source $$++ takeUntil boundary =$ sink
-
-    if b == "--"
+    if B.null bs
         then return ()
-        else sendEach boundary s sink
+        else do
+            isOpen' <- isOpen
+            if isOpen'
+                then case B.breakSubstring boundary bs of
+                    (beginning, ending) -> do
+                        leftover ending
+                        yield (EndSection, beginning)
+                        takeUntil boundary
+                    otherwise -> do
+                        leftover $ B.drop (bl * 2) bs
+                        yield $ (Continue, B.take (bl * 2) bs)
+                        takeUntil boundary
+                else yield (EndContent, bs) >> return ()
+
+    -- try this:
+    -- take 3x boundary length
+    -- if empty
+    --   return
+    -- if less than expected length or we can tell we're at the end of the stream
+    --   yield it all
+    -- if needle exists
+    --   then leftover snd, yield fst
+    --   else leftover (length of boundary), yield rest
+
+    -- case () of
+    --     _ | BL.null bs -> return ()
+    --       | toStrict bs == boundary -> return ()
+    --       | otherwise -> do
+    --           yield $ toStrict $ BL.singleton $ BL.head bs
+    --           leftover $ toStrict $ BL.tail bs
+
+    --           takeUntil boundary
+
+-- sendEach :: MonadIO m
+--          => ByteString
+--          -> ResumableSource m ByteString
+--          -> Sink ByteString m ()
+--          -> m ()
+-- sendEach boundary source sink =  do
+--     (s, b) <- source $$++ takeUntil boundary =$ sink
+
+    -- if b == "--"
+    --     then return ()
+    --     else sendEach boundary s sink
 
     -- d <- isDone source
 
@@ -58,3 +86,7 @@ sendEach boundary source sink =  do
 
 toStrict :: BL.ByteString -> ByteString
 toStrict = B.concat . BL.toChunks
+
+isOpen :: (MonadIO m) => ConduitM i o m Bool
+isOpen = await >>= maybe (return False) ((True <$) . leftover)
+-- isOpen = CL.peek >>= maybe (return False) (return True)
--- src/Run.hs
+++ src/Run.hs
@@ -1,36 +1,99 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Run where
 
 import Accum
 
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Resource (MonadResource, runResourceT)
 import Data.ByteString (ByteString)
+import Data.ByteString.Internal (c2w)
 import Data.Conduit
 import Data.Conduit.Binary
 import Data.UUID
 import Data.UUID.V4
 import Network.Wai (Request)
 import Network.Wai.Conduit (sourceRequestBody)
+import System.IO (openFile, hClose, Handle, IOMode(WriteMode))
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 
 runRequest :: ByteString -> Request -> IO ()
 runRequest boundary req = runResourceT $ do
+    sourceRequestBody req $= takeUntil boundary $$ multiSinkFile
+    return ()
+
+    -- sourceRequestBody req $= removeVowels $$ sinkFile'
     -- b <- sourceRequestBody req $$ sinkLbs
+    -- liftIO $ print boundary
+    -- liftIO $ print "runRequest: "
     -- liftIO $ BL.putStr b
+    -- let rsb = newResumableSource $ sourceRequestBody req
 
-    -- sourceRequestBody req $= takeUntil boundary $$ sinkPrint
-    sendEach boundary (newResumableSource $ sourceRequestBody req) sinkPrint
+    -- (rsb', d) <- rsb $$++ takeUntil boundary =$ sinkPart
+    -- liftIO $ print d
+    -- liftIO $ print "-----------"
+    -- (rsb'', d') <- rsb' $$++ consumePart boundary
+    -- liftIO $ print d'
+    -- liftIO $ print "-----------"
+    -- (rsb''', d'') <- rsb'' $$++ consumePart boundary
+    -- liftIO $ print d''
+    -- liftIO $ print "-----------"
 
-saveFile :: MonadResource m => Sink ByteString m ()
-saveFile = do
-    fn <- liftIO $ fmap toString $ nextRandom
-    sinkFile ("out/" ++ fn)
+    -- forEachPart boundary rsb sinkFile'
+    -- return ()
 
-sinkPrint :: MonadResource m => Sink ByteString m ()
-sinkPrint = do
+    -- sendEach boundary (newResumableSource $ sourceRequestBody req) sinkPrint
+
+multiSinkFile :: MonadResource m => Sink (Progress, ByteString) m ()
+multiSinkFile = multiSinkFile' newFile
+
+multiSinkFile' :: MonadResource m => IO Handle -> Sink (Progress, ByteString) m ()
+multiSinkFile' f = do
     mv <- await
     case mv of
-        Just v -> liftIO (B.putStr v) >> sinkPrint
-        Nothing -> liftIO (putStrLn "") >> return ()
+        Nothing -> return ()
+        Just (s, d) -> case s of
+            Continue -> do
+                liftIO $ iohPutStr f d
+                multiSinkFile' f
+            EndSection -> do
+                liftIO $ iohPutStr f d
+                liftIO $ iohClose f
+                multiSinkFile' newFile
+            EndContent -> do
+                liftIO $ iohPutStr f d
+                liftIO $ iohClose f
+
+iohClose :: IO Handle -> IO ()
+iohClose ioh = ioh >>= hClose
+
+iohPutStr :: IO Handle -> ByteString -> IO ()
+iohPutStr ioh s = ioh >>= flip B.hPutStr s
+
+newFile :: IO Handle
+newFile = do
+    fn <- liftIO $ fmap toString $ nextRandom
+    openFile fn WriteMode
+
+
+-- forEachPart :: MonadIO m
+--                => ByteString
+--                -> ResumableSource m ByteString
+--                -> Sink ByteString m ()
+--                -> m ()
+-- forEachPart boundary source sink = do
+--     (source', _) <- source $$++ takeUntil boundary =$ sink
+--     forEachPart boundary source' sink
+
+-- sinkFile' :: MonadResource m => Sink ByteString m ()
+-- sinkFile' = do
+--     fn <- liftIO $ fmap toString $ nextRandom
+--     sinkFile ("out/" ++ fn)
+
+-- sinkPrint :: MonadResource m => Sink ByteString m ()
+-- sinkPrint = do
+--     mv <- await
+--     case mv of
+--         Just v -> liftIO (B.putStr v) >> sinkPrint
+--         Nothing -> liftIO (putStrLn "") >> return ()
