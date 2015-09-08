49103c77be8c969c5a3f6a1ed2100c0c69aba951
Jon Yurek <jyurek@thoughtbot.com>
2015-09-08 09:47:42 -0400

    Remove ResumableRun

--- src/ResumableRun.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Streamer.ResumableRun where
-
-runRequest :: ByteString -> Request -> IO ()
-runRequest boundary req = runResourceT $ do
-    newResumableSource $ sourceRequestBody req
